e8893d3d400c0c564bf98762f38573c27eb938ba
codeplaneteer <softechstar.module@outlook.com>
2023-08-07 22:38:55 -0300

    [WIP] Update telegram.py

--- src/telegram.py
+++ src/telegram.py
@@ -1,4 +1,5 @@
 import os
+import json
 import requests
 from dotenv import load_dotenv
 
@@ -14,18 +15,24 @@ class Telegram:
     def __init__(self):
         load_dotenv()
         self.__api_key = os.environ.get('API_KEY')
+        self.__chat_id = os.environ.get('CHAT_ID')
 
     @property
     def api_key(self) -> str:
         return self.__api_key
 
-    def send_message(self):
-        query = f'https://api.telegram.org/bot{self.api_key}/functionsss'
+    @property
+    def chat_id(self) -> str:
+        return self.__chat_id
+
+    def send_message(self, text):
+        query = f'https://api.telegram.org/bot{self.api_key}/sendMessage?chat_id={self.chat_id}&text={text}'
         try:
-            res = requests.post(query)
+            res = requests.get(query)
+            print(res.json())
             if res.status_code != 200:
                 raise Exception(f'Error: status code {res.status_code}')
         except Exception as e:
             msg = f'Error: {e}'
-            return msg
-
+            print(msg)
+        return res.json()
